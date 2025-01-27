90538335dfd6119af6d363e53889d504ca72c7af
Zick_Zibn <karpenko_ilia@list.ru>
2025-01-28 00:29:39 +0300

    commit

--- src/test/java/SimpleCacheTest.java
+++ src/test/java/SimpleCacheTest.java
@@ -10,7 +10,7 @@ public class SimpleCacheTest {
         LimitedCache<String, String> limitedCache = new LimitedCache<>(7);
 
         limitedCache.put("One", "Test1");
-        limitedCache.put("One", "Test1");
+        limitedCache.put("Two", "Test2");
         assertEquals(limitedCache.size(), 2);
     }
 }
