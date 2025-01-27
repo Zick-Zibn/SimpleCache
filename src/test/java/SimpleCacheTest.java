ab5278a557e7f266e4057a99b857c66bd362c3f1
Zick_Zibn <karpenko_ilia@list.ru>
2025-01-28 00:01:45 +0300

    add to test put

--- src/test/java/SimpleCacheTest.java
+++ src/test/java/SimpleCacheTest.java
@@ -1,8 +1,11 @@
+import org.example.LimitedCache;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class SimpleCacheTest {
     @Test
     public void LimitedCacheTestPut(){
+        LimitedCache<String, String> limitedCache = new LimitedCache<>(7);
 
+        limitedCache.put("One", "Test1");
     }
 }
