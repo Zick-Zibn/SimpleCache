fdcc22c732671920188d2383297c8ecc093148fd
Zick_Zibn <karpenko_ilia@list.ru>
2025-01-28 05:08:06 +0300

    commit all

--- src/test/java/SimpleCacheTest.java
+++ src/test/java/SimpleCacheTest.java
@@ -1,11 +1,12 @@
 import org.example.Filter;
 import org.example.LimitedCache;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Assert;
 
 public class SimpleCacheTest {
 
@@ -15,7 +16,7 @@ public class SimpleCacheTest {
 
         limitedCache.put("One", "Test1");
         limitedCache.put("Two", "Test2");
-        assertEquals(limitedCache.size(), 2);
+        Assert.assertEquals(limitedCache.size(), 2);
     }
     @Test
     public void LimitedCacheTestGet() {
@@ -23,8 +24,8 @@ public class SimpleCacheTest {
 
         limitedCache.put("One", "Test1");
         limitedCache.put("Two", "Test2");
-        assertEquals(limitedCache.get("One"), "Test1");
-        assertEquals(limitedCache.get("Three"), null);
+        Assert.assertEquals(limitedCache.get("One"), "Test1");
+        Assert.assertEquals(limitedCache.get("Three"), null);
     }
     @Test
     public void LimitedCacheTestContainsKey() {
@@ -32,8 +33,8 @@ public class SimpleCacheTest {
 
         limitedCache.put("One", "Test1");
         limitedCache.put("Two", "Test2");
-        assertEquals(limitedCache.containsKey("One"), true);
-        assertEquals(limitedCache.containsKey("Three"), false);
+        Assert.assertEquals(limitedCache.containsKey("One"), true);
+        Assert.assertEquals(limitedCache.containsKey("Three"), false);
     }
     @Test
     public void LimitedCacheTestSize() {
@@ -41,13 +42,13 @@ public class SimpleCacheTest {
 
         limitedCache.put("One", "Test1");
         limitedCache.put("Two", "Test2");
-        assertEquals(limitedCache.size(), 2);
+        Assert.assertEquals(limitedCache.size(), 2);
     }
     @Test
     public void FilterTest(){
         List<String> src = List.of("qwert", "venrm", "sff", "ssss", "gbvdavAV", "ryrehewe");
         List<String> dest = new ArrayList<>();
         Filter.copyIf(src, dest, o -> o.length() > 4);
-        assertEquals(dest.size(), 4);
+        Assert.assertEquals(dest.size(), 4);
     }
 }
